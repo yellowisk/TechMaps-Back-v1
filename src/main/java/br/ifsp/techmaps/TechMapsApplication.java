413e6ef62e6a126dd8ed8b04683469d5c485c40d
Heitor Yellowisk <heitor.a@aluno.ifsp.edu.br>
2023-04-15 00:44:53 -0300

    Project Setup: Spring initializr

---
+++ src/main/java/br/ifsp/techmaps/TechMapsApplication.java
@@ -0,0 +1,13 @@
+package br.ifsp.techmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMapsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechMapsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ifsp/techmaps/TechMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifsp.techmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMapsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
