e4c08392612120fe376bce4b0fbd4ef8a046ab47
yellowisk <heitor.a@aluno.ifsp.edu.br>
2023-08-02 22:31:20 -0300

    feat: adding CRUD methods for Step

--- src/main/java/br/ifsp/techmaps/usecases/step/StepCRUD.java
+++ src/main/java/br/ifsp/techmaps/usecases/step/StepCRUD.java
@@ -1,4 +1,21 @@
 package br.ifsp.techmaps.usecases.step;
 
+import br.ifsp.techmaps.domain.entities.step.Step;
+
+import java.util.*;
+
 public interface StepCRUD {
+
+    Step addNewTaskSteps(UUID taskId);
+
+    List<Step> getStepsByTaskId(UUID taskId);
+
+    Step updateStepNumber(Step step);
+
+    Step prioritizeTaskStep(Step step);
+
+    Step finishTaskStep(Step step);
+
+    Step deleteTaskStep(UUID stepId);
+
 }
--- src/main/java/br/ifsp/techmaps/usecases/step/StepCRUDImpl.java
+++ src/main/java/br/ifsp/techmaps/usecases/step/StepCRUDImpl.java
@@ -1,4 +1,49 @@
 package br.ifsp.techmaps.usecases.step;
 
+import br.ifsp.techmaps.domain.entities.step.Step;
+import br.ifsp.techmaps.usecases.step.gateway.StepDAO;
+import br.ifsp.techmaps.usecases.task.gateway.TaskDAO;
+
+import java.util.List;
+import java.util.UUID;
+
 public class StepCRUDImpl implements StepCRUD {
+
+    private final StepDAO stepDAO;
+    private final TaskDAO taskDAO;
+
+    public StepCRUDImpl(StepDAO stepDAO, TaskDAO taskDAO) {
+        this.stepDAO = stepDAO;
+        this.taskDAO = taskDAO;
+    }
+
+    @Override
+    public Step addNewTaskSteps(UUID taskId) {
+        return null;
+    }
+
+    @Override
+    public List<Step> getStepsByTaskId(UUID taskId) {
+        return null;
+    }
+
+    @Override
+    public Step updateStepNumber(Step step) {
+        return null;
+    }
+
+    @Override
+    public Step prioritizeTaskStep(Step step) {
+        return null;
+    }
+
+    @Override
+    public Step finishTaskStep(Step step) {
+        return null;
+    }
+
+    @Override
+    public Step deleteTaskStep(UUID stepId) {
+        return null;
+    }
 }
